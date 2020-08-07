8ed9f59ed01a81ad40646acab6e8f24626085eb1
InterHorse <mayuzhe1222@qq.com>
2020-08-07 16:48:26 +0800

    fix: 修改注释

--- src/main/java/cn/interhorse/springboot/bjtu/dao/mapper/MileageDetailsMapper.java
+++ src/main/java/cn/interhorse/springboot/bjtu/dao/mapper/MileageDetailsMapper.java
@@ -14,29 +14,29 @@ public interface MileageDetailsMapper {
      * 查询年龄区间
      *
      * @param startBirthYear 起始年龄
-     * @param endBirthYear 结束年龄
+     * @param endBirthYear   结束年龄
      * @return
      */
     @Select("select * from mileage_details_t where birth_year >= #{start} and birth_year <= #{end}")
     List<MileageDetails> selectByYearsInterval(@Param("start") int startBirthYear, @Param("end") int endBirthYear);
 
     /**
-     * 查询年龄区间
+     * 查询里程区间
      *
-     * @param startBirthYear 起始年龄
-     * @param endBirthYear 结束年龄
+     * @param startMileage 起始里程
+     * @param endMileage   结束里程
      * @return
      */
     @Select("select * from mileage_details_t where total_mileage >= #{start} and total_mileage <= #{end}")
-    List<MileageDetails> selectByMilesInterval(@Param("start") int startBirthYear, @Param("end") int endBirthYear);
+    List<MileageDetails> selectByMilesInterval(@Param("start") int startMileage, @Param("end") int endMileage);
 
     /**
-     * 查询年龄区间
+     * 查询时间区间
      *
-     * @param startBirthYear 起始年龄
-     * @param endBirthYear 结束年龄
+     * @param startTime 起始时间
+     * @param endTime   结束时间
      * @return
      */
     @Select("select * from mileage_details_t where total_time >= #{start} and total_time <= #{end}")
-    List<MileageDetails> selectByTimeInterval(@Param("start") int startBirthYear, @Param("end") int endBirthYear);
+    List<MileageDetails> selectByTimeInterval(@Param("start") int startTime, @Param("end") int endTime);
 }
