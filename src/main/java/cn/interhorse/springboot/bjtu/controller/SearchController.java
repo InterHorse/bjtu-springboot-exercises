98533f207ac8d7e92a59f0e9045af235b7839f28
InterHorse <mayuzhe1222@qq.com>
2020-08-20 18:44:11 +0800

    style: 去掉无用引用

--- src/main/java/cn/interhorse/springboot/bjtu/controller/SearchController.java
+++ src/main/java/cn/interhorse/springboot/bjtu/controller/SearchController.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 
 /**
